80d9454be9a147ae324744031d4fdda30d8fb3ca
Skillers <Rick.nl@hotmail.com>
2026-04-13 10:57:15 +0200

    Hover brush wokring

--- Assets/LevelEditor/BrushController.cs
+++ Assets/LevelEditor/BrushController.cs
@@ -22,18 +22,77 @@ public class BrushController : MonoBehaviour
     bool _rightBlocked;  // ignore right until physically released
     GameObject _indicator;
 
+    const int RingSegments = 64;
+    const float RingThickness = 0.15f; // fraction of radius
+
+    Mesh _ringMesh;
+    Vector3[] _baseInner;
+    Vector3[] _baseOuter;
+
     void Start()
     {
-        _indicator = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        _indicator.name = "BrushIndicator";
-        _indicator.transform.localScale = Vector3.one * 5f;
-        Destroy(_indicator.GetComponent<Collider>());
+        // Pre-compute unit circle directions
+        _baseInner = new Vector3[RingSegments];
+        _baseOuter = new Vector3[RingSegments];
+        float inner = 1f - RingThickness;
 
-        var mr = _indicator.GetComponent<MeshRenderer>();
+        for (int i = 0; i < RingSegments; i++)
+        {
+            float angle = (float)i / RingSegments * Mathf.PI * 2f;
+            float cos = Mathf.Cos(angle);
+            float sin = Mathf.Sin(angle);
+            _baseInner[i] = new Vector3(cos * inner, 0f, sin * inner);
+            _baseOuter[i] = new Vector3(cos, 0f, sin);
+        }
+
+        // Build initial mesh with triangles (verts updated each frame)
+        var tris = new int[RingSegments * 6];
+        for (int i = 0; i < RingSegments; i++)
+        {
+            int next = (i + 1) % RingSegments;
+            int t = i * 6;
+            tris[t]     = i * 2;
+            tris[t + 1] = next * 2;
+            tris[t + 2] = i * 2 + 1;
+            tris[t + 3] = next * 2;
+            tris[t + 4] = next * 2 + 1;
+            tris[t + 5] = i * 2 + 1;
+        }
+
+        _ringMesh = new Mesh { name = "BrushRing" };
+        _ringMesh.MarkDynamic();
+        _ringMesh.vertices = new Vector3[RingSegments * 2];
+        _ringMesh.triangles = tris;
+
+        _indicator = new GameObject("BrushIndicator");
+        _indicator.AddComponent<MeshFilter>().sharedMesh = _ringMesh;
+        var mr = _indicator.AddComponent<MeshRenderer>();
         var shader = Shader.Find("Universal Render Pipeline/Lit") ?? Shader.Find("Standard");
         mr.sharedMaterial = new Material(shader) { color = indicatorColor };
-
         _indicator.SetActive(false);
+    }
+
+    void UpdateRingToTerrain(Vector3 center)
+    {
+        var verts = new Vector3[RingSegments * 2];
+        float offset = 0.15f; // hover above surface
+
+        for (int i = 0; i < RingSegments; i++)
+        {
+            Vector3 innerWorld = center + _baseInner[i] * BrushRadius;
+            Vector3 outerWorld = center + _baseOuter[i] * BrushRadius;
+
+            innerWorld.y = terrainDataStore.GetHeight(innerWorld) + offset;
+            outerWorld.y = terrainDataStore.GetHeight(outerWorld) + offset;
+
+            // Store in local space (indicator at origin)
+            verts[i * 2]     = innerWorld;
+            verts[i * 2 + 1] = outerWorld;
+        }
+
+        _ringMesh.vertices = verts;
+        _ringMesh.RecalculateNormals();
+        _ringMesh.RecalculateBounds();
     }
 
     public void ToggleBrush()
@@ -58,7 +117,9 @@ public class BrushController : MonoBehaviour
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
             _indicator.SetActive(true);
-            _indicator.transform.position = hit.point;
+            _indicator.transform.position = Vector3.zero;
+            _indicator.transform.localScale = Vector3.one;
+            UpdateRingToTerrain(hit.point);
         }
         else
         {
