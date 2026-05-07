35b0daacb89e88c5f08a8fe5516fcfc9032143ac
Elias Youssef <eliasyoussef47@gmail.com>
2026-05-07 11:39:17 +0200

    Changes selection highlighting.

--- Assets/Scripts/PlacedObstacle.cs
+++ Assets/Scripts/PlacedObstacle.cs
@@ -3,22 +3,33 @@ using UnityEngine;
 public class PlacedObstacle : MonoBehaviour
 {
     private static readonly int BaseColor = Shader.PropertyToID("_BaseColor");
+    private static readonly int EmissionColor = Shader.PropertyToID("_EmissionColor");
 
     public ObstacleSO obstacleSO;
 
     private Renderer _renderer;
     private Material _materialInstance;
     private Color _originalColor;
+    private Color _originalEmission;
 
     private void Awake()
     {
         _renderer = GetComponentInChildren<Renderer>();
         _materialInstance = _renderer.material;
         _originalColor = _materialInstance.GetColor(BaseColor);
+        _materialInstance.EnableKeyword("_EMISSION");
+        _originalEmission = _materialInstance.GetColor(EmissionColor);
     }
 
     public void SetSelected(bool selected)
     {
-        _materialInstance.SetColor(BaseColor, selected ? Color.yellow : _originalColor);
+        if (selected)
+        {
+            _materialInstance.SetColor(EmissionColor, Color.cyan * 0.1f);
+        }
+        else
+        {
+            _materialInstance.SetColor(EmissionColor, _originalEmission);
+        }
     }
 }
